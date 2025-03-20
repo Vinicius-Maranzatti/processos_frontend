e805e6488a1a7b1d3e9670a7d41c37df61f1835c
Vinicius-Maranzatti <enzomaranzattisilva@gmail.com>
2025-03-19 22:31:51 -0300

    teste

--- modal_preview.php
+++ modal_preview.php
@@ -3,13 +3,6 @@
     <h3 class="text-lg font-bold mb-4">Arquivos Processados</h3>
 
     <div>
-        <!-- Interface de processamento -->
-        <div id="loading" style="display: none;">
-            <div class="loader"></div> <!-- Animação de loading -->
-            <p>Processando...</p>
-            <p>Arquivos <span id="arquivoAtual">0</span> de <span id="totalArquivos">0</span> concluídos.</p>
-            <div id="erros" style="color: red;"></div>
-        </div>
 
         <div id="steps">
           <!-- Animação de passos -->
@@ -21,7 +14,6 @@
           </ul>
         </div>
         
-
         <!-- Barra de progresso -->
         <div class="progress-bar">
             <div class="progress-bar-inner"></div>
@@ -29,8 +21,7 @@
 
         <!-- Feedback de conclusão -->
         <div id="conclusao" style="display: none;">
-            <p>Processamento concluído com sucesso!</p>
-            <i class="fas fa-check-circle check-icon"></i>
+            <p><i class="fas fa-check-circle check-icon"></i> Processamento concluído com sucesso!</p>
         </div>
 
         <!-- Lista de arquivos processados -->
