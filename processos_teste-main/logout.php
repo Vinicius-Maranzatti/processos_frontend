98da035dc4e67dacdc4dec2880581a1c4e5fdb05
Vinicius-Maranzatti <enzomaranzattisilva@gmail.com>
2025-03-19 21:57:16 -0300

    Delete processos_teste-main directory

--- processos_teste-main/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    session_destroy();  // Destroi todas as variáveis de sessão
-    header("Location: ../index.php");  // Redireciona para a página inicial
-    exit;
-?>
